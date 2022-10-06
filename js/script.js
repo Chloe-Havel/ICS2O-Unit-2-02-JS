677d026810dafb93efc25c4ea04bc4d1ec852d34
Chloe-Havel <Chloe-Havel@users.noreply.github.com>
2022-10-06 18:47:09 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -9,6 +9,8 @@
  * @constructor
  */
 function doMathClicked() {
-  document.getElementById("multiply-math").innerHTML = "<p>Area =  " + (5 * 3) + "</p>" 
-  document.getElementById("add-math").innerHTML = "<p>perimeter = " + (2 * (5 + 3)) + "</p>"
+  document.getElementById("multiply-math").innerHTML =
+    "<p>Area =  " + 5 * 3 + "</p>"
+  document.getElementById("add-math").innerHTML =
+    "<p>perimeter = " + 2 * (5 + 3) + "</p>"
 }
